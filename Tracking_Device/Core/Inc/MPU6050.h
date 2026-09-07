a3e55af7bf26ffa922b8d82fb8e515966b3fdc16
Sakith2002 <sakithnethkalana@gmail.com>
2026-09-07 22:01:19 +0530

    Initial commit

---
+++ Tracking_Device/Core/Inc/MPU6050.h
@@ -0,0 +1,11 @@
+#ifndef MPU6050_H
+#define MPU6050_H
+
+#include "stm32f1xx_hal.h"
+
+void MPU6050_init(void); //Initialize the MPU 
+void MPU6050_Config_Interrupt(I2C_HandleTypeDef *hi2c); //Configure the Interrupt of MPU
+void MPU6050_Read_Accel (float *Ax, float *Ay, float *Az); //Read MPU Accelerator 
+void MPU6050_Read_Gyro (float *Gx, float *Gy, float *Gz); //Read MPU Gyroscope
+
+#endif /* MPU6050_H */
